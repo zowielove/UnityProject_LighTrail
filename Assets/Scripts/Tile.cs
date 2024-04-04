4156a4e94484a42b95bf12513a2438940031f1b5
sumeing.vogi@gmail.com <sumeing.vogi@gmail.com>
2024-04-04 09:52:10 +0900

    feat : 플레이어, 타일
    코드, 테스트용 소스

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] Renderer rendererToChangeMaterial;
+    [SerializeField] BoxCollider collder;
+    [SerializeField] Material material;
+
+    public bool play;
+
+    private void OnTriggerEnter( Collider collision )
+    {
+        if ( ( layerMask.value & 1 << collision.gameObject.layer ) != 0 )
+        {
+            play = true;
+            if ( rendererToChangeMaterial != null && material != null )
+            {
+                rendererToChangeMaterial.material = material;
+            }
+        }
+    }
+
+    private void OnTriggerExit( Collider collision )
+    {
+        
+    }
+}
