608e0704f53e1b61b6abbb67dda19a597783a92b
sumeing.vogi@gmail.com <sumeing.vogi@gmail.com>
2024-04-05 13:32:03 +0900

    feat : 카메라스크립트 수정

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    [SerializeField] CinemachineVirtualCamera camera_1;
-    [SerializeField] CinemachineVirtualCamera camera_2;
+    [SerializeField] CinemachineVirtualCamera camera_now;
+    [SerializeField] CinemachineVirtualCamera camera_next;
     [SerializeField] LayerMask layerMask;
 
     private void OnTriggerEnter( Collider collision )
     {
         if ( ( layerMask.value & 1 << collision.gameObject.layer ) != 0 )
         {
-            camera_2.Priority = 11;
+            camera_now.Priority = 9;
+            camera_next.Priority = 11;
         }
     }
 }
