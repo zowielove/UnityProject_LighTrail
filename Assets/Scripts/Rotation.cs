63a9046d65778602510c13eca43e0f33bb846665
sumeing.vogi@gmail.com <sumeing.vogi@gmail.com>
2024-04-09 10:16:41 +0900

    feat : 포탈

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float rotationSpeed = 200f; // 회전 속도
+
+    void Update()
+    {
+        // 오브젝트를 z축 주위로 회전시키기
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
