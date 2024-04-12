0824382bebb6610dc2fae5727a061b8e1c51a642
Kyd0312 <71116531+zowielove@users.noreply.github.com>
2024-04-12 21:03:19 +0900

    Update Fri

---
+++ Assets/NeonSignStage/Scripts/ReStart_NeonSign.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ReStart : MonoBehaviour
+{
+    public void GameSceneLoad()
+    {
+
+        StartCoroutine(LoadGameScene());
+    }
+
+    public IEnumerator LoadGameScene()
+    {
+
+        yield return new WaitForSecondsRealtime(1.5f);
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Neon_Sign");
+
+    }
+}
