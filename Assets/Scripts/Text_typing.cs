a1f611c79c57f59948819ebfa88cdc9bbeb91675
sumeing.vogi@gmail.com <sumeing.vogi@gmail.com>
2024-04-12 17:27:04 +0900

    feat : 타이틀 타이핑텍스트

---
+++ Assets/Scripts/Text_typing.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class Text_typing : MonoBehaviour
+{
+    public TMP_Text teamname;
+    [SerializeField] string text;
+    [SerializeField] float fadeTime;
+
+    void Start()
+    {
+        text = $"{text}";
+        StartCoroutine(Typing(text));
+        
+    }
+
+    IEnumerator Typing(string typing)
+    {
+        teamname.text = null;
+        for(int i = 0; i< typing.Length; i++ )
+        {
+            teamname.text += typing [i];
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(FaFadeOut());
+    }
+
+    IEnumerator FaFadeOut()
+    {
+        float rate = 0;
+        Color fadeOutColor = new Color(teamname.color.r, teamname.color.g, teamname.color.b, 0f);
+        Color fadeInColor = new Color(teamname.color.r, teamname.color.g, teamname.color.b, 1f);
+
+        while ( rate <= 1 )
+        {
+            rate += Time.deltaTime / fadeTime;
+            teamname.color = Color.Lerp(fadeInColor, fadeOutColor, rate);
+            yield return null;
+        }
+        yield return new WaitForSeconds(3f);
+    }
+}
