655027734f2f77dd01f44ef9b0f6f95ed9aca52a
gazamhi <kid3060@gmail.com>
2024-04-12 18:43:26 +0900

    Tropical

--- Assets/Scripts/ReStart.cs
+++ Assets/Scripts/ReStart.cs
@@ -16,7 +16,7 @@ public class ReStart : MonoBehaviour
 
         yield return new WaitForSecondsRealtime(1.5f);
 
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Test_JB");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Tropical");
 
     }
 }
