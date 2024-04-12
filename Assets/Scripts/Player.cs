47611ee37d1bd5da667a430f0d1b00404ddb82f2
sumeing.vogi@gmail.com <sumeing.vogi@gmail.com>
2024-04-12 17:39:15 +0900

    feat : 플레이어 수정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
     [SerializeField] Transform trans;    
     private bool isActionInProgress = false;
     [SerializeField] float actionCoolTime;
+    
+
 
     [Header("Judgment")]
     [SerializeField] LayerMask leftTurnCheck;
